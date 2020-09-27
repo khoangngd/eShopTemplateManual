c0d33b24f97755d8c2f25ed7bde6a1f0b00ad003
Home's  KN Tumlum Media <40882242+KNTumlumMedia@users.noreply.github.com>
2020-09-27 21:06:12 +0700

    ef+ajs

--- eShop.Web/App_Start/BundleConfig.cs
+++ eShop.Web/App_Start/BundleConfig.cs
@@ -25,6 +25,11 @@ namespace eShop.Web
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                       "~/Scripts/angular.min.js"
+                       
+                       ));
         }
     }
 }
